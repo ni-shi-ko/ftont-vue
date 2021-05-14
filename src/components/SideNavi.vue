8123dbbe5264f521f4f785345b0fbe2ca650044c
”こうしろう” <nishideko0824@gmail.com>
2021-05-14 16:18:55 +0900

    first

---
+++ src/components/SideNavi.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="left-padding">
+      <img src="../assets/assets/logo.png" class="logo" />
+      <div class="flex icon-field" @click="$router.push('/home')">
+        <img src="../assets/assets/home.png" class="icon" />
+        <p>ホーム</p>
+      </div>
+      <div class="flex icon-field" @click="$router.push('/profile')">
+        <img src="../assets/assets/profile.png" class="icon" />
+        <p>プロフィール</p>
+      </div>
+      <div class="flex icon-field" @click="$router.dispatch('logout')">
+        <img src="../assets/assets/logout.png"  class="icon" />
+        <p>ログアウト</p>
+      </div>
+    </div>
+    <ShareMessage />
+  </div>
+</template>
+
+<script>
+import ShareMessage from "../components/ShareMessage";
+export default {
+  components : {
+    ShareMessage
+  }
+};
+</script>
+
+<style scoped>
+  .left-padding {
+    margin: 20px;
+  }
+  .flex {
+    display: flex;
+  }
+  .logo {
+    width: 100px;
+  }
+  .icon-field {
+    margin-top: 15px;
+    cursor: pointer;
+  }
+  .icon {
+    width: 25px;
+  }
+  .icon-field p {
+    font-size: 16px;
+    padding-left: 15px;
+  }
+</style>
